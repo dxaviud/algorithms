29a64aa71f6d76896dc1353d0f969551db5faf91
David Xu <dxaviud@gmail.com>
2020-12-13 15:45:25 -0500

    BubbleSort

--- Java/Sort/BubbleSort.java
+++ Java/Sort/BubbleSort.java
@@ -2,10 +2,14 @@ public class BubbleSort {
     
     public static void sort(int[] a)
     {
-        for (int i = 0; i < a.length - 2; i++)
+        int i = 0;
+        for (; i < a.length - 1 && a[i] <= a[i+1]; i++);
+        int jInit = i;
+        for (; i < a.length - 1; i++)
         {
-            for (int j = i + 1; j < a.length - 1; j++)
+            for (int j = jInit; j < a.length - 1 - i; j++)
             {
+                System.out.println("i: " + i + " j: " + j);
                 if (a[j] > a[j+1])
                 {
                     swap(a, j, j + 1);
@@ -32,6 +36,8 @@ public class BubbleSort {
     public static void main(String[] args) 
     {
         int[] array = {9,8,7,6,5,4,3,2,1,0};
+        printArray(array);
+        sort(array);
         printArray(array);
         sort(array);
         printArray(array);
