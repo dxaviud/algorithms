9d615475c03654362e5dc48dfa52f6504bb41223
David Xu <dxaviud@gmail.com>
2021-01-10 16:24:10 -0500

    refactor

--- Java/ArrayList/ArrayList.java
+++ Java/ArrayList/ArrayList.java
@@ -4,77 +4,55 @@ public class ArrayList<T> {
     private int capacity;
     private int size;
 
-    public ArrayList()
-    {
+    public ArrayList() {
         size = 0;
         capacity = 2;
         array = new Object[capacity];
     }
 
-    public void add(T data)
-    {
-        if (size == capacity)
-            grow();
-        array[size] = data;
-        size++;
-    }
-
-    public T get(int index)
-    {
-        if (index < 0 || index >= size)
-            throw new IndexOutOfBoundsException("Invalid index: " + index);
+    public T get(int index) {
+        if (index < 0 || index >= size) throw new IndexOutOfBoundsException("Invalid index: " + index);
         @SuppressWarnings("unchecked")
         final T t = (T) array[index];
         return t;
     }
 
-    public T remove(int index)
-    {
-        if (index < 0 || index >= size)
-            return null;
-        T t = get(index);
-        for (int i = index; i < size - 1; i++)
-        {
-            array[i] = array[i+1];
-        }
-        size--;
-        if (size < capacity/2)
-            shrink();
-        return t;
+    public void add(T data) {
+        if (size == capacity) grow();
+        array[size] = data;
+        size++;
     }
 
-    public int size()
-    {
-        return size;
-    }
-
-    private void grow()
-    {
+    private void grow() {
         Object[] array2 = new Object[capacity*2];
-        for (int i = 0; i < capacity; i++)
-        {
-            array2[i] = array[i];
-        }
+        for (int i = 0; i < capacity; i++) array2[i] = array[i];
         array = array2;
         capacity *= 2;
     }
 
-    private void shrink()
-    {
+    public T remove(int index) {
+        if (index < 0 || index >= size) throw new IndexOutOfBoundsException("Invalid index: " + index);
+        T t = get(index);
+        for (int i = index; i < size - 1; i++) array[i] = array[i+1];
+        size--;
+        if (size < capacity/2) shrink();
+        return t;
+    }
+
+    private void shrink() {
         Object[] array2 = new Object[capacity/2];
-        for (int i = 0; i < capacity/2; i++)
-        {
-            array2[i] = array[i];
-        }
+        for (int i = 0; i < capacity/2; i++) array2[i] = array[i];
         array = array2;
         capacity /= 2;
     }
 
-    public void print()
-    {
+    public int size() {
+        return size;
+    }
+
+    public void print() {
         System.out.print("[");
-        for (int i = 0; i < size; i++)
-        {
+        for (int i = 0; i < size; i++) {
             if (i == size - 1)
                 System.out.print(array[i]);
             else
--- Java/ArrayList/TestArrayList.java
+++ Java/ArrayList/TestArrayList.java
@@ -1,14 +1,12 @@
 public class TestArrayList {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
 
         ArrayList<Integer> alist = new ArrayList<Integer>();
         alist.print();
         System.out.println("size: " + alist.size() + "\n");
 
         //testing add
-        for (int i = 0; i < 5; i++)
-        {
+        for (int i = 0; i < 5; i++) {
             System.out.println("Adding " + i);
             alist.add(i);
             alist.print();
@@ -17,8 +15,7 @@ public class TestArrayList {
         System.out.println();
 
         //testing remove at an index
-        for (int i = 4; i >= 0; i--)
-        {
+        for (int i = 4; i >= 0; i--) {
             System.out.println("Removing index " + i);
             alist.remove(i);
             alist.print();
@@ -27,15 +24,13 @@ public class TestArrayList {
         System.out.println();
 
         System.out.println("Adding 0 to 9");
-        for (int i = 0; i < 10; i++)
-        {
+        for (int i = 0; i < 10; i++) {
             alist.add(i);
         }
         alist.print();
 
         //testing get
-        for (int i = 0; i < 10; i++)
-        {
+        for (int i = 0; i < 10; i++) {
             System.out.println("Data at index " + i + ": " + alist.get(i));
         }
     }    
