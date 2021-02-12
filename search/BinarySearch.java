21c8f8425a4f0f6ec0f8d7424dc516304e97f6d7
David Xu <dxaviud@gmail.com>
2021-02-12 18:21:32 -0500

    package

--- search/BinarySearch.java
+++ search/BinarySearch.java
@@ -1,30 +1,27 @@
-public class BinarySearch
-{
-    //Just for int arrays, I'm too noob to make it generic right now
-    public static int binarySearch(int[] array, int value)
-    {
+package search;
+
+public class BinarySearch {
+
+    //Just for int arrays right now
+    public static int binarySearch(int[] array, int value) {
         int low = 0;
         int high = array.length - 1;
         int mid = (low + high)/2;
-        while (low <= high)
-        {
-            if (value > array[mid])
-            {
+        while (low <= high) {
+            if (value > array[mid]) {
                 low = mid + 1;
                 mid = (low + high)/2;
-            }
-            else if (value < array[mid])
-            {
+            } else if (value < array[mid]) {
                 high = mid - 1;
                 mid = (low + high)/2;
+            } else {
+                return mid;
             }
-            else return mid;
         }
         return -1;
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         int[] array = {0,1,2,3,4,5,6,7,8,9};
         for (int i : array)
             System.out.print(i + " ");
@@ -32,4 +29,5 @@ public class BinarySearch
         int toFind = 5;
         System.out.println("Index of " + toFind + ": " + binarySearch(array, toFind));
     }
+
 }
