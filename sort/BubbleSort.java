f694d1a8c719dab3fc3fc5a988dbaa1dc26df33c
David Xu <dxaviud@gmail.com>
2021-02-12 18:25:35 -0500

    {

--- sort/BubbleSort.java
+++ sort/BubbleSort.java
@@ -1,46 +1,41 @@
+package sort;
+
 public class BubbleSort {
     
-    public static void sort(int[] a)
-    {
+    public static void sort(int[] a) {
         int i = 0;
         for (; i < a.length - 1 && a[i] <= a[i+1]; i++);
         int jInit = i;
-        for (; i < a.length - 1; i++)
-        {
-            for (int j = jInit; j < a.length - 1 - i; j++)
-            {
+        for (; i < a.length - 1; i++) {
+            for (int j = jInit; j < a.length - 1 - i; j++) {
                 System.out.println("i: " + i + " j: " + j);
-                if (a[j] > a[j+1])
-                {
+                if (a[j] > a[j+1]) {
                     swap(a, j, j + 1);
                 }
             }
         }
     }
 
-    private static void swap(int[] a, int i, int j)
-    {
+    private static void swap(int[] a, int i, int j) {
         int temp = a[i];
         a[i] = a[j];
         a[j] = temp;
     }
 
-    private static void printArray(int[] a)
-    {
-        for (int i : a) 
-        {
+    private static void printArray(int[] a) {
+        for (int i : a) {
             System.out.print(i + " ");
         }
         System.out.println();
     }
-    public static void main(String[] args) 
-    {
+
+    public static void main(String[] args) {
         int[] array = {9,8,7,6,5,4,3,2,1,0};
         printArray(array);
         sort(array);
         printArray(array);
         sort(array);
         printArray(array);
-
     }
+    
 }
